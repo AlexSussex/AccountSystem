0ee1ba61725472c4e2f40e196aafab6403c834c9
AlexSussex <ap576@sussex.ac.uk>
2018-04-28 16:39:25 +0100

    added project structure and testing for adding an account

---
+++ src/test/java/accountSystem/AccountMapServiceTest.java
@@ -0,0 +1,33 @@
+package accountSystem;
+
+import static org.junit.Assert.*;
+import org.alex.domain.Account;
+import org.alex.util.JSONUtil;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class AccountMapServiceTest {
+
+	AccountMapService accounts;
+	
+	private Account account1;
+	private Account account2;
+	private JSONUtil util;
+	
+	@Before
+	public void init() {
+		accounts = new AccountMapService();
+		account1 = new Account("Alex", "Developer", "12750589");
+		account2 = new Account("Tib", "Flat", "16542389");
+		util = new JSONUtil();
+	}
+	
+	@Test
+	public void addAccountTest() {
+		Assert.assertEquals("{\"message\": \"account sucessfully added\"}", accounts.addAccount(util.getJSONForObject(account1)));
+		Assert.assertEquals("{\"message\": \"account sucessfully added\"}", accounts.addAccount(util.getJSONForObject(account2)));
+	}
+}
